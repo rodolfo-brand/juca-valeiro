8e86a428b95f4ac699eaf1f1de48d5e0d40d4397
rodolfo-brand <rnbrandalise@gmail.com>
2025-06-04 11:14:54 -0300

    added score

--- scripts/0.5_coin.gd
+++ scripts/0.5_coin.gd
@@ -1,5 +1,7 @@
 extends Area2D
 
-func _on_body_entered(_body: Node2D) -> void:
-	print("+1 coin!")
+@onready var game_manager: Node = %GameManager
+
+func _on_body_entered(_body: Node2D):
+	game_manager.add_point()
 	queue_free() 
